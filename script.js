136300bd2bc1c7fd0eb91cd20dcea9c5c8a1ff43
Hernando <hernandoabella@gmail.com>
2020-10-10 17:44:37 -0400

    adding some changes

--- script.js
+++ script.js
@@ -11,3 +11,22 @@ function myFunction() {
 
 // scroll indicator
 
+
+$(document).ready(function () {
+  $('#sidebarCollapse').on('click', function () {
+      $('#sidebar').toggleClass('active');
+      $(this).toggleClass('active');
+  });
+});
+
+function openSideDrawer() {
+  document.getElementById("side-drawer").style.left = "0";
+  document.getElementById("side-drawer-void").classList.add("d-block");
+  document.getElementById("side-drawer-void").classList.remove("d-none");
+}
+
+function closeSideDrawer() {
+  document.getElementById("side-drawer").style.left = "-336px";
+  document.getElementById("side-drawer-void").classList.add("d-none");
+  document.getElementById("side-drawer-void").classList.remove("d-block");
+}
